44f4b9d71d81a6a651162dadc5d99eedda9e33eb
Sebastian Denis <sebastian.denis@outlook.com>
2022-01-13 23:50:02 +0100

    lesson 75

--- components/events/event-item.tsx
+++ components/events/event-item.tsx
@@ -1,6 +1,10 @@
 import Image from 'next/image';
-import Link from 'next/link';
 import { IEvent } from '../../dummy-data';
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
+import DateIcon from '../icons/date-icon';
+import Button from '../ui/button';
+import classes from './event-item.module.css';
 
 const EventItem = (props: { event: IEvent }) => {
   const { event } = props;
@@ -15,20 +19,32 @@ const EventItem = (props: { event: IEvent }) => {
   const exploreLink = `/events/${event.id}`;
 
   return (
-    <li>
-      <Image src={'/' + event.image} alt={event.title} width={300} height={200}/>
-      <div>
-        <div>
+    <li className={classes.item}>
+      <Image
+        src={'/' + event.image}
+        alt={event.title}
+        width={300}
+        height={200}
+      />
+      <div className={classes.content}>
+        <div className={classes.summary}>
           <h2>{event.title}</h2>
-          <div>
+          <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
-          <div>
+          <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
-        <div>
-          <Link href={exploreLink}>Explore Event</Link>
+        <div className={classes.actions}>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
--- components/events/event-list.tsx
+++ components/events/event-list.tsx
@@ -1,15 +1,15 @@
 import { IEvent } from '../../dummy-data';
 import EventItem from './event-item';
+import classes from './event-list.module.css';
 
 const EventList = (props: { items: Array<IEvent> }) => {
   const { items } = props;
 
   return (
-    <ul>
+    <ul className={classes.list}>
       {items.map((item: IEvent) => (
         <EventItem event={item} key={item.id} />
       ))}
-      ;
     </ul>
   );
 };
