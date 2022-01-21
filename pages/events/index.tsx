7d59caa5d9b6844c15f6f02d9776ba4f134815f0
Sebastian Denis <sebastian.denis@outlook.com>
2022-01-21 16:18:10 +0100

    lesson 80

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from 'next';
+import { useRouter } from 'next/router';
 import { Fragment } from 'react';
 import EventList from '../../components/events/event-list';
 import EventsSearch from '../../components/events/events-search';
@@ -6,9 +7,16 @@ import { getAllEvents } from '../../dummy-data';
 
 const Events: NextPage = () => {
   const events = getAllEvents();
+  const router = useRouter();
+
+  const findEventsHandler = (year: HTMLSelectElement, month: HTMLSelectElement) => {
+    const fullPath = `/events/${year.value}/${month.value}`;
+    router.push(fullPath);
+  };
+
   return (
     <Fragment>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler}/>
       <EventList items={events} />
     </Fragment>
   );
